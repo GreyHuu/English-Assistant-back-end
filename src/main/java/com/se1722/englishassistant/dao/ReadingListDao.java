1d18d450cd5288d09d75ab175434be5143f7c87e
GreyHuhu <1519715742@qq.com>
2020-06-08 14:49:37 +0800

    合并金金

--- src/main/java/com/se1722/englishassistant/dao/ReadingListDao.java
+++ src/main/java/com/se1722/englishassistant/dao/ReadingListDao.java
@@ -1,10 +1,13 @@
 package com.se1722.englishassistant.dao;
 
+import com.se1722.englishassistant.entity.ReadingListEntity;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 @Mapper
 @Component
@@ -17,6 +20,24 @@ public interface ReadingListDao {
      */
     @Select("select count(*) as num from reading_list where group_id = #{id}")
     int selectCountByGroupID(Integer id);
+
+    /**
+     * 获得某个用户的全部的练习次数
+     *
+     * @param id
+     * @return
+     */
+    @Select("select count(*) as num from reading_list where user_id = #{id}")
+    int selectCountByUserID(Integer id);
+
+    /**
+     * 获取某个题目组的全部记录
+     *
+     * @param id
+     * @return
+     */
+    @Select("select *  from reading_list where group_id = #{id}")
+    List<ReadingListEntity> selectReadingListByGroupID(Integer id);
 
     /**
      * 获取某个题目组的最高分数
@@ -35,4 +56,31 @@ public interface ReadingListDao {
      */
     @Select("select max(time)  from reading_list where group_id = #{id}")
     Date selectLastTimeByGroupID(Integer id);
+
+    /**
+     * 插入历史记录
+     *
+     * @param readingListEntity
+     * @return
+     */
+    @Insert("INSERT INTO reading_list(id,group_id,user_id,score,time,during_time) VALUES(#{id},#{group_id},#{user_id},#{score},#{time},#{during_time})")
+    Integer insertReadingList(ReadingListEntity readingListEntity);
+
+    /**
+     * 获得ReadingListEntity
+     *
+     * @param id
+     * @return
+     */
+    @Select("select * from reading_list where id = #{id}")
+    ReadingListEntity getById(Integer id);
+
+    /**
+     * 获得用户的id
+     *
+     * @param id
+     * @return
+     */
+    @Select("select * from reading_list where user_id = #{id}")
+    List<ReadingListEntity> getAllListByUserId(Integer id);
 }
--- src/main/java/com/se1722/englishassistant/service/impl/ReadingListImpl.java
+++ src/main/java/com/se1722/englishassistant/service/impl/ReadingListImpl.java
@@ -1,16 +1,23 @@
 package com.se1722.englishassistant.service.impl;
 
 import com.se1722.englishassistant.dao.ReadingListDao;
+import com.se1722.englishassistant.entity.ReadingListEntity;
 import com.se1722.englishassistant.service.ReadingListService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class ReadingListImpl implements ReadingListService {
     @Resource
     private ReadingListDao readingListDao;
+
+    @Override
+    public List<ReadingListEntity> getAllListByUserId(Integer id) {
+        return readingListDao.getAllListByUserId(id);
+    }
 
     @Override
     public int selectCountByGroupID(Integer id) {
@@ -25,5 +32,25 @@ public class ReadingListImpl implements ReadingListService {
     @Override
     public Date selectLastTimeByGroupID(Integer id) {
         return readingListDao.selectLastTimeByGroupID(id);
+    }
+
+    @Override
+    public Integer insertReadingList(ReadingListEntity readingListEntity) {
+        return readingListDao.insertReadingList(readingListEntity);
+    }
+
+    @Override
+    public ReadingListEntity getById(Integer id) {
+        return readingListDao.getById(id);
+    }
+
+    @Override
+    public int selectCountByUserID(Integer id) {
+        return readingListDao.selectCountByUserID(id);
+    }
+
+    @Override
+    public List<ReadingListEntity> selectReadingListByGroupID(Integer id) {
+        return readingListDao.selectReadingListByGroupID(id);
     }
 }
