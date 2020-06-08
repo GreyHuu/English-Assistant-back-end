14da2903bdbd8eb98f695d2282281c1a5a9e4f51
BanburyKing <yaozunjin@qq.com>
2020-06-08 14:16:20 +0800

    存档

--- src/main/java/com/se1722/englishassistant/web/CompositionController.java
+++ src/main/java/com/se1722/englishassistant/web/CompositionController.java
@@ -2,15 +2,20 @@ package com.se1722.englishassistant.web;
 
 import com.se1722.englishassistant.entity.CompositionBankEntity;
 import com.se1722.englishassistant.entity.CompositionEntity;
+import com.se1722.englishassistant.entity.CurrentUser;
 import com.se1722.englishassistant.service.CompositionBankService;
 import com.se1722.englishassistant.service.CompositionService;
 import com.se1722.englishassistant.utils.RestResponse;
+import com.se1722.englishassistant.utils.SessionContent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /*
  * 作者：姚尊金
@@ -21,11 +26,46 @@ import java.util.Map;
 @RestController
 @RequestMapping("/writing")
 public class CompositionController {
+    // 获取当前session
+    private static final String CURRENT_USER_SESSION = "current_session";
+    //登录后的用户ID
+    private Integer user_id ;
+
     @Resource
     private CompositionBankService compositionBankService;
     @Resource
     private CompositionService compositionService;
 
+    /**
+     * 获取当前用户的ID
+     * @param request
+     * @return user_id
+     */
+    private Integer getUserID(HttpServletRequest request){
+        String sessionId = request.getHeader("Session_Id");
+        //获取当前session
+        HttpSession session = SessionContent.getSession(sessionId);
+        //获取当前用户信息
+        CurrentUser user = (CurrentUser) session.getAttribute(CURRENT_USER_SESSION);
+
+        return user.getId();
+    }
+    public Integer getScore(int wordCount) {
+        int baseScore = 70;
+        int randomScore = new Random().nextInt(10) + 1;
+
+        if(wordCount <= 100)
+            return baseScore + randomScore + new Random().nextInt(5) + 1;
+        else if (wordCount <= 150)
+            return baseScore + randomScore + new Random().nextInt(10) + 1;
+        else if (wordCount <= 250)
+            return baseScore + randomScore + new Random().nextInt(15) + 1;
+        else if (wordCount <= 400)
+            return baseScore + randomScore + new Random().nextInt(20) + 1;
+        else
+            return baseScore + randomScore + new Random().nextInt(5) + 1;
+
+    }
     /**
      *获取全部作文题目
      */
@@ -46,14 +86,17 @@ public class CompositionController {
 //    400错误
 //    form-data、x-www-form-urlencoded：需要使用@RequestParam
 //    application/json： 需要使用@RequestBody
-    @PostMapping("/add-a-composition-and-count")
-//    public RestResponse AddCompositionAndCount(CompositionEntity mycpt, Integer cpt_reference){
-    public RestResponse AddCompositionAndCount(@RequestBody String jsonStr){
-        System.out.println(jsonStr);
-//        int num = compositionService.addACompositionAndCount(
-//                (CompositionEntity) map.get("mycpt"),
-//                (Integer) map.get("cpt_reference"));
-         int num=0;
+    @PostMapping("/add-a-composition-and-count/{cpt_reference}")
+    public RestResponse AddCompositionAndCount(
+            @PathVariable("cpt_reference") Integer cpt_reference,
+            @RequestBody CompositionEntity mycpt,
+            HttpServletRequest request){
+
+        user_id = getUserID(request);
+        mycpt.setUser_id(user_id);
+        mycpt.setMark(mycpt.getMycpt_word_count());
+
+        int num = compositionService.addACompositionAndCount(mycpt, cpt_reference);
 
         if(num == 1) {
             return RestResponse.succuess("添加成功");
