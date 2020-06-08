c12d144922a124af187de87066eab183b00ac1a2
ZZZGGGG <1691124661@qq.com>
2020-06-08 09:35:44 +0800

    news and the comments

--- src/main/java/com/se1722/englishassistant/entity/NewsCommentsEntity.java
+++ src/main/java/com/se1722/englishassistant/entity/NewsCommentsEntity.java
@@ -11,8 +11,12 @@ public class NewsCommentsEntity {
 
     //评论内容
     private String comment;
+    //用户id
     private Integer user_id;
+    //新闻id
     private Integer new_id;
+    //评论id
+    private Integer c_id;
 
     public String getComment() {
         return comment;
@@ -32,5 +36,6 @@ public class NewsCommentsEntity {
     public void setNew_id(Integer new_id) {
         this.new_id = new_id;
     }
-
+    public Integer getC_id() { return c_id; }
+    public void setC_id(Integer c_id) { this.c_id = c_id; }
 }
